27780997622bd288e80426be8110754b04c2a32e
Max Scheiber <keasbeynumb@gmail.com>
2013-08-08 20:27:47 -0700

    solve 3.4-3.5, Java

--- src/com/maxscheiber/ctci/Chapter3.java
+++ src/com/maxscheiber/ctci/Chapter3.java
@@ -3,6 +3,7 @@ package com.maxscheiber.ctci;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Stack;
 
 public class Chapter3 {
@@ -178,6 +179,153 @@ public class Chapter3 {
 				throw new IllegalArgumentException("Cannot pop from an uninitialized stack");
 			}
 			return stacks.get(index).pop();
+		}
+	}
+	
+	/**
+	 * Implement Towers of Hanoi. 3.4
+	 * @param N number of disks to start with
+	 * @return the target stack, representing the finished game
+	 */
+	public static Stack<Integer> towersOfHanoi(int N) {
+		Stack<Integer> s1 = new Stack<Integer>();
+		Stack<Integer> s2 = new Stack<Integer>();
+		Stack<Integer> s3 = new Stack<Integer>();
+
+		if (N <= 0) {
+			throw new IllegalArgumentException("Must have a positive number "
+					+ "of disks to play Towers of Hanoi");
+		}
+
+		// initialize s1 with the appropriate integers
+		for (int i = N; i >= 1; i--) {
+			s1.push(i);
+		}
+
+		move(N, s1, s2, s3);
+		/*
+		 * System.out.print("Stack 1: "); printStack(s1);
+		 * System.out.print("Stack 2: "); printStack(s2);
+		 * System.out.print("Stack 3: "); printStack(s3);
+		 */
+
+		return s3;
+	}
+	
+	/**
+	 * Moves the top element of one stack onto another stack.
+	 * @param src stack to pop from
+	 * @param tgt stack to push onto
+	 */
+	private static void move(Stack<Integer> src, Stack<Integer> tgt) {
+		if (tgt.empty()) {
+			// System.out.println("Moving " + src.peek() + " to empty stack");
+		} else {
+			// System.out.println("Moving " + src.peek() + " on top of " +
+			// tgt.peek());
+		}
+		tgt.push(src.pop());
+	}
+     /**
+      * Given a stack of N consecutive integer disks, move them from src pole
+      * to tgt pole, using mid pole for transfers.
+      * @param N number of consecutive integer disks (i.e. no skips)
+      * @param src starting pole to move from
+      * @param mid pole used for passing
+      * @param tgt target pole to move to
+      */
+	private static void move(int N, Stack<Integer> src, Stack<Integer> mid,
+			Stack<Integer> tgt) {
+		if (N == 1) {
+			move(src, tgt);
+			return;
+		}
+		move((N - 1), src, tgt, mid);
+		move(src, tgt);
+		move((N - 1), mid, src, tgt);
+		if (tgt.size() == N) {
+			return;
+		}
+	}
+    
+    /**
+     * Prints the stack, top to bottom.
+     * @param s stack to print
+     */
+	private static void printStack(Stack<Integer> s) {
+		Stack<Integer> cp = new Stack<Integer>();
+		while (!s.empty()) {
+			cp.push(s.pop());
+			System.out.print(cp.peek() + " ");
+		}
+		while (!cp.empty()) {
+			s.push(cp.pop());
+		}
+		System.out.print("\n");
+	}
+	
+	/**
+	 * Implement a MyQueue class which implements a queue using two stacks. 3.5
+	 */
+	public static class MyQueue<A> {
+		Stack<A> head = new Stack<A>();
+		Stack<A> tail = new Stack<A>();
+		
+		public boolean add(A v) {
+			tail.push(v);
+			return true;
+		}
+		
+		public A element() {
+			if (head.empty()) {
+				transfer();
+			}
+			try {
+				return head.peek();
+			} catch (EmptyStackException e) {
+				throw new NoSuchElementException();
+			}
+		}
+		
+		public boolean offer(A v) {
+			return add(v);
+		}
+		
+		public A peek() {
+			try {
+				return this.element();
+			} catch (NoSuchElementException e) {
+				return null;
+			}
+		}
+		
+		public A poll() {
+			try {
+				return this.remove();
+			} catch (NoSuchElementException e) {
+				return null;
+			}
+		}
+		
+		public A remove() {
+			if (head.empty()) {
+				transfer();
+			}
+			try {
+				return head.pop();
+			} catch (EmptyStackException e) {
+				throw new NoSuchElementException();
+			}
+		}
+		
+		public int size() {
+			return head.size() + tail.size();
+		}
+		
+		private void transfer() {
+			while (!tail.empty()) {
+				head.push(tail.pop());
+			}
 		}
 	}
 }
--- src/com/maxscheiber/ctci/Chapter3Test.java
+++ src/com/maxscheiber/ctci/Chapter3Test.java
@@ -2,14 +2,18 @@ package com.maxscheiber.ctci;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.EmptyStackException;
+import java.util.NoSuchElementException;
+import java.util.Stack;
 
 import org.junit.Test;
 
 import com.maxscheiber.ctci.Chapter3.MinStack;
+import com.maxscheiber.ctci.Chapter3.MyQueue;
 import com.maxscheiber.ctci.Chapter3.SetOfStacks;
 import com.maxscheiber.ctci.Chapter3.ThreeStackArray;
 
@@ -107,5 +111,54 @@ public class Chapter3Test {
 		} catch (EmptyStackException e) {
 			assertTrue("Correctly caught empty stack pop()", true);
 		}
+	}
+	
+	@Test
+	public void testTowersOfHanoi() {
+		try {
+			Chapter3.towersOfHanoi(0);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue("Cannot play Hanoi with <= 0 disks", true);
+		}
+		
+		for (int n = 1; n <= 10; n++) {
+			Stack<Integer> hanoi = Chapter3.towersOfHanoi(n);
+			assertEquals("Finished " + n + "-Hanoi with proper number of disks", 
+					n, hanoi.size());
+			for (int i = 1; i <= n; i++) {
+				assertTrue("Expected to pop" + i, i == hanoi.pop());
+			}
+		}
+	}
+	
+	@Test
+	public void testMyQueue() {
+		MyQueue<Integer> q = new MyQueue<Integer>();
+		try {
+			q.element();
+			fail();
+		} catch (NoSuchElementException e) {
+			assertTrue("Throw exception on empty element()", true);
+		}
+		assertNull("Null on empty peek()", q.peek());
+		
+		try {
+			q.remove();
+			fail();
+		} catch (NoSuchElementException e) {
+			assertTrue("Throw exception on empty remove()", true);
+		}
+		assertNull("Null on empty poll()", q.poll());
+		
+		q.add(1);
+		q.add(2);
+		q.add(3);
+		assertTrue("remove 1", 1 == q.remove());
+		assertTrue("element 2", 2 == q.element());
+		assertTrue("poll 2", 2 == q.poll());
+		assertTrue("peek 3", 3 == q.peek());
+		assertTrue("remove 3", 3 == q.remove());
+		assertEquals("empty queue", 0, q.size());
 	}
 }
