b4757fa78768f47b8ff1661e5e6886be5c1cbe1b
Max Scheiber <keasbeynumb@gmail.com>
2013-08-09 00:57:32 -0700

    solve 4.1-4.3, Java

--- src/com/maxscheiber/ctci/Chapter4.java
+++ src/com/maxscheiber/ctci/Chapter4.java
@@ -1,5 +1,10 @@
 package com.maxscheiber.ctci;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
 public class Chapter4 {
 	public static class BST {
 		private int v;
@@ -25,6 +30,46 @@ public class Chapter4 {
 		
 		public BST r() {
 			return r;
+		}
+	}
+	
+	public static int[] inorder(BST t) {
+		if (t == null) {
+			return new int[0];
+		} else if (isLeaf(t)) {
+			return new int[]{t.v};
+		}
+		int[] l = inorder(t.l);
+		int[] r = inorder(t.r);
+		int[] out = new int[l.length + 1 + r.length];
+		System.arraycopy(l, 0, out, 0, l.length);
+		out[l.length] = t.v;
+		System.arraycopy(r, 0, out, l.length + 1, r.length);
+		return out;
+	}
+	
+	public enum State {
+		NEW, CURRENT, OLD
+	}
+	
+	public static class Node<V> {
+		private V val;
+		private Set<Node<V>> edges;
+		private State state;
+		
+		public Node(V val) {
+			this.val = val;
+			this.edges = new HashSet<Node<V>>();
+			state = State.NEW;
+		}
+		
+		public void directedEdge(Node<V> v) {
+			this.edges.add(v);
+		}
+		
+		public void undirectedEdge(Node<V> v) {
+			this.directedEdge(v);
+			v.directedEdge(this);
 		}
 	}
 	
@@ -65,6 +110,14 @@ public class Chapter4 {
 			return t;
 		}
 	}
+	
+	public static BST insertArray(int[] xs, BST t) {
+		for (int x : xs) {
+			t = insert(x, t);
+		}
+		return t;
+	}
+	
 	 /**
 	  * Delete from a binary search tree
 	  * @param x value to delete
@@ -100,5 +153,116 @@ public class Chapter4 {
 	private static int min(BST t) {
 		while (t.l != null) t = t.l;
 		return t.v;
+	}
+	
+	/**
+	 * Implement a function to check if a tree is balanced. For the purposes of
+	 * this question, a balanced tree is defined to be a tree such that no two
+	 * leaf nodes differ in distance from the root by more than one. 4.1
+	 * @param t tree to check for balance
+	 * @return true if t is balanced, false otherwise
+	 */
+	public static boolean isBalanced(BST t) {
+		if (t == null) {
+			return false;
+		}
+		
+		// base case - given a leaf
+		if (isLeaf(t)) {
+			return true;
+		}
+		
+		// base case - left is one above right
+		if (t.l == null && isLeaf(t.r)) {
+			return true;
+		}
+		
+		// base case - right is one above left
+		if (isLeaf(t.l) && t.r == null) {
+			return true;
+		}
+		
+		return Math.abs(depth(t.l) - depth(t.r)) <= 1 && 
+				isBalanced(t.l) && isBalanced(t.r);
+	}
+	
+	public static int depth(BST t) {
+		return depth(t, 0);
+	}
+	
+	private static int depth(BST t, int n) {
+		if (t == null) {
+			return n;
+		}
+		return Math.max(depth(t.l, n+1), depth(t.r, n+1));
+	}
+	
+	private static boolean isLeaf(BST t) {
+		if (t == null) {
+			return false;
+		}
+		return t.l == null && t.r == null;
+	}
+	
+	/**
+	 * Given a directed graph, design an algorithm to find out whether there is
+	 * a route between two nodes. 4.2
+	 * @param s starting node
+	 * @param t ending node
+	 * @return true if there exists a path from s to t, false otherwise
+	 */
+	public static <V> boolean pathExists(Node<V> s, Node<V> t) {
+		if (s == null || t == null) {
+			return false;
+		}
+		
+		Queue<Node<V>> q = new LinkedList<Node<V>>();
+		s.state = State.OLD;
+		q.add(s);
+		
+		// iterative DFS
+		while (!q.isEmpty()) {
+			Node<V> u = q.remove();
+			for (Node<V> v : u.edges) {
+				if (t == v) {
+					return true;
+				} else if (v.state == State.NEW) {
+					v.state = State.OLD;
+					q.add(v);
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	public static <V> void resetVisits(Node<V>... nodes) {
+		for (Node<V> node : nodes) {
+			node.state = State.NEW;
+		}
+	}
+	
+	/**
+	 * Given a sorted (increasing order) array, write an algorithm to create a
+	 * binary tree with minimal height. 4.3
+	 * @param xs sorted array to convert into a BST
+	 * @return minimal-depth BST
+	 */
+	public static BST fromArray(int[] xs) {
+		if (xs == null) {
+			return null;
+		}
+		
+		return divideBST(0, xs.length-1, xs);
+	}
+	
+	private static BST divideBST(int lo, int hi, int[] xs) {
+		if (lo == hi) {
+			return new BST(xs[lo]);
+		} else if (lo > hi) {
+			return null;
+		}
+		int mid = lo + (hi-lo)/2;
+		return new BST(divideBST(lo, mid-1, xs), xs[mid], divideBST(mid+1, hi, xs));
 	}
 }
--- src/com/maxscheiber/ctci/Chapter4Test.java
+++ src/com/maxscheiber/ctci/Chapter4Test.java
@@ -1,11 +1,14 @@
 package com.maxscheiber.ctci;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import com.maxscheiber.ctci.Chapter4.BST;
+import com.maxscheiber.ctci.Chapter4.Node;
 
 public class Chapter4Test {
 	@Test
@@ -21,5 +24,78 @@ public class Chapter4Test {
 		assertEquals("Node is 3", 3, t.v());
 		assertEquals("Left is 1", 1, t.l().v());
 		assertNull("Right is null", t.r());
+	}
+	
+	@Test
+	public void testIsBalanced() {
+		assertFalse("Null tree", Chapter4.isBalanced(null));
+		assertTrue("Singleton tree", Chapter4.isBalanced(new BST(0)));
+		assertTrue("Two-element tree", Chapter4.isBalanced(Chapter4.insert(1, 
+				new BST(0))));
+		assertTrue("Three-element balanced tree", Chapter4.isBalanced(
+				Chapter4.insertArray(new int[]{2, 1, 3}, null)));
+		assertTrue("Four-element balanced tree", Chapter4.isBalanced(
+				Chapter4.insertArray(new int[]{2, 1, 3, 4}, null)));
+		assertFalse("Queue tree", Chapter4.isBalanced(
+				Chapter4.insertArray(new int[]{1, 2, 3}, null)));
+		assertFalse("Large unbalanced tree", Chapter4.isBalanced(
+				Chapter4.insertArray(new int[]{0, -2, -3, -1, 10, 5, 15, 3, 8, 
+						12, 18, 14}, null)));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testPathExists() {
+		Node<Integer> n1 = new Node<Integer>(1);
+		Node<Integer> n2 = new Node<Integer>(2);
+		Node<Integer> n3 = new Node<Integer>(3);
+		Node<Integer> n4 = new Node<Integer>(4);
+		Node<Integer> n5 = new Node<Integer>(5);
+		Node<Integer> n6 = new Node<Integer>(6);
+		Node<Integer> n7 = new Node<Integer>(7);
+		Node<Integer> n8 = new Node<Integer>(8);
+		n1.directedEdge(n2);
+		n1.directedEdge(n3);
+		n2.directedEdge(n4);
+		n2.directedEdge(n5);
+		n3.directedEdge(n6);
+		n3.directedEdge(n7);
+		n5.directedEdge(n4);
+		n8.directedEdge(n4);
+		
+		assertTrue("path from 1 to 2", Chapter4.pathExists(n1, n2));
+		Chapter4.resetVisits(n1, n2, n3, n4, n5, n6, n7, n8);
+		assertTrue("path from 1 to 3", Chapter4.pathExists(n1, n3));
+		Chapter4.resetVisits(n1, n2, n3, n4, n5, n6, n7, n8);
+		assertTrue("path from 1 to 4", Chapter4.pathExists(n1, n4));
+		Chapter4.resetVisits(n1, n2, n3, n4, n5, n6, n7, n8);
+		assertTrue("path from 1 to 7", Chapter4.pathExists(n1, n5));
+		Chapter4.resetVisits(n1, n2, n3, n4, n5, n6, n7, n8);
+		assertFalse("no path from 1 to 8", Chapter4.pathExists(n1, n8));
+		Chapter4.resetVisits(n1, n2, n3, n4, n5, n6, n7, n8);
+	}
+	
+	@Test
+	public void testFromArray() {
+		assertNull("Null array", Chapter4.fromArray(null));
+		testFromArray(new int[]{1});
+		testFromArray(new int[]{1, 2});
+		testFromArray(new int[]{1, 2, 3});
+		testFromArray(new int[]{1, 2, 3, 4});
+		testFromArray(new int[]{1, 2, 3, 4, 5});
+		testFromArray(new int[]{1, 2, 3, 4, 5, 6});
+		testFromArray(new int[]{1, 2, 3, 4, 5, 6, 7});
+		testFromArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
+	}
+	
+	private void testFromArray(int[] xs) {
+		BST t = Chapter4.fromArray(xs);
+		int[] back = Chapter4.inorder(t);
+		int N = xs.length;
+		for (int i = 0; i < N; i++) {
+			assertEquals("Legnth " + N, xs[i], back[i]);
+		}
+		assertEquals("Length " + N, 1 + Math.ceil((int) (Math.log(N) / Math.log(2))), 
+				Chapter4.depth(t), 0.01);
 	}
 }
